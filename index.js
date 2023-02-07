29a0d3287b1eaf2d8460e181718a61875db27a9c
6rak0 <ego.6rak0@gmail.com>
2023-02-06 22:12:04 -0600

    fix: :bug: changed hour to UTC

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, {polling: true});
 const chatId = process.env.CHAT_ID;
 
+const products = [
+    'https://petco.com.mx/petco/en/PRODUCTOS/Perro/EveryYay-Cama-Circular-Ortop%C3%A9dica-con-Infusi%C3%B3n-de-Lavanda-Calmante-para-Perro%2C-Mediano/p/137751',
+    'https://petco.com.mx/petco/en/MARCAS/Kurgo/Kurgo-Correa-Quantum-Azul-Multiproposito-6-en-1-para-Perro%2C-1-8-m/p/114474'
+]
+
+// function wait(ms){
+//     return new Promise(resolve => setTimeout(resolve, ms))
+// }
+
 async function getPetcoData(url) {
     try {
         const response = await axios(url)
@@ -13,6 +22,7 @@ async function getPetcoData(url) {
         const $ = cheerio.load(html)
         const product = $('span#name-category').text()
         const price = $('.discountedPrice').text()
+        //const img = `https://petco.com.mx${$('#imagePreviewContainerData a').attr('href')}`
         return {product, price}
     } catch (error) {
         console.error(error)
@@ -20,14 +30,26 @@ async function getPetcoData(url) {
     }
 }
 
-const sendMessage = (id, message) => {
-    bot.sendMessage(id, message)
-    .catch(error => console.log(error));
+const sendMessage = async (id, message) => {
+    try {
+        //await bot.sendPhoto(id, url)
+        await bot.sendMessage(id, message)
+    } catch (error) {
+        console.error(error)
+    }
 };
 
-async function notification() {
-    const {product, price} = await getPetcoData('https://petco.com.mx/petco/en/PRODUCTOS/Perro/EveryYay-Cama-Circular-Ortop%C3%A9dica-con-Infusi%C3%B3n-de-Lavanda-Calmante-para-Perro%2C-Mediano/p/137751')
-    sendMessage(chatId, `${product} está en ${price}`)
+async function notification(url) {
+    const {product, price} = await getPetcoData(url)
+    await sendMessage(chatId, `${product} está en ${price}`)
 }
 
-cron.schedule('0 9 * * *', () => notification())
+// products.forEach(product => {
+//     notification(product)
+// })
+
+cron.schedule('0 15 * * *', () => {
+    products.forEach(product => {
+        notification(product)
+    })
+})
