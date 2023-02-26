440591e129817a41e48ea382c315f3c7b405ef62
6rak0 <ego.6rak0@gmail.com>
2023-02-26 14:07:07 -0600

    feat: :sparkles: implemented add and remove items

--- index.js
+++ index.js
@@ -1,23 +1,76 @@
 import dotenv from 'dotenv';
 import cron from 'node-cron';
-import cheerio from 'cheerio';
+import {load} from 'cheerio';
 import axios from 'axios';
 import TelegramBot from 'node-telegram-bot-api';
+import Pocketbase from 'pocketbase'
 
 dotenv.config();
 const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, {polling: true});
 const chatId = process.env.CHAT_ID;
+const pb = new Pocketbase(process.env.POCKETBASE_URL)
 
-const products = [
-    'https://petco.com.mx/petco/en/PRODUCTOS/Perro/EveryYay-Cama-Circular-Ortop%C3%A9dica-con-Infusi%C3%B3n-de-Lavanda-Calmante-para-Perro%2C-Mediano/p/137751',
-    'https://petco.com.mx/petco/en/MARCAS/Kurgo/Kurgo-Correa-Quantum-Azul-Multiproposito-6-en-1-para-Perro%2C-1-8-m/p/114474'
-];
+const getItems = async () => {
+    const items = await pb.collection('petco_items').getFullList({
+        sort: '-created',
+    }); 
+    return items
+}
+
+const addItem = async (url) => {
+    try {
+        await pb.collection('petco_items').create({url})
+        console.log('✅ item added')
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+const removeItem = async (url) => {
+    const items = await getItems()
+    items.forEach(async item => {
+        if (url === item.url){
+            try {
+                await pb.collection('petco_items').delete(item.id)
+                console.log('💣 item removed')
+            } catch (error) {
+                console.error(error)
+            }
+        }
+    })
+}
+
+// add
+bot.onText(/\/add https?:\/\/(?:www\.)?petco\.com\.mx(?:\/.*)/,async (msg, match) => {
+    const chatId = msg.chat.id;
+    const url = match[0].split(' ')[1]
+    try {
+        await addItem(url)
+        bot.sendMessage(chatId, `✅ el producto se agregó de manera correcta`)
+    } catch (error) {
+        console.error(error)
+        bot.sendMessage(chatId, `❌ el producto no se agregó debido a esto: ${error}`)
+    }
+});
+
+// delete
+bot.onText(/\/del https?:\/\/(?:www\.)?petco\.com\.mx(?:\/.*)/,async (msg, match) => {
+    const chatId = msg.chat.id;
+    const url = match[0].split(' ')[1]
+    try {
+        await removeItem(url)
+        bot.sendMessage(chatId, `✅ el producto se borró de manera correcta`)
+    } catch (error) {
+        console.error(error)
+        bot.sendMessage(chatId, `❌ el producto no se borró debido a esto: ${error}`)
+    }
+});
 
 const getPetcoData = async (url) => {
     try {
         const response = await axios(url);
         const html = response.data;
-        const $ = cheerio.load(html);
+        const $ = load(html);
         const product = $('span#name-category').text();
         const price = $('.discountedPrice').text();
         //const img = `https://petco.com.mx${$('#imagePreviewContainerData a').attr('href')}`
@@ -42,7 +95,8 @@ const notification = async (url) => {
     await sendMessage(chatId, `${product} está en ${price}`);
 };
 
-cron.schedule('0 15 * * *', () => {
+cron.schedule('55 13 * * *',async () => {
+    const products = await getItems()
     products.forEach(product => {
         notification(product);
     });
